1b2f490707686125ce9c9933a6c0c4001080d067
kyoohyun <kgh2120@naver.com>
2023-04-27 18:43:54 +0900

    JS로 내 위치 긁어오기 추가

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,15 @@
 
-X
-(src/main/java/com/kk/wifi/vo/WifiVO.java,0\b\0be786f0e9f2048c334db012beac34835929ecf9
 O
 src/main/webapp/WEB-INF/web.xml,2\e\2e22b6edd746fa3dc40ad7276c87c8d87d72a392
 i
 9src/main/java/com/kk/wifi/controller/HelloController.java,e\7\e769870ab0c532c4fd8833b412f4fe7339b5cca5
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+W
+'src/main/webapp/WEB-INF/views/Hello.jsp,3\e\3eb9832f32118b5eea142dfd1bd70de0b4d85e40
+]
+-src/main/java/com/kk/wifi/servlet/MyView.java,0\b\0b92845e6756b26471573994a53d56366d72c154
+d
+4src/main/java/com/kk/wifi/controller/Controller.java,b\2\b22b22a70fd091031f15b833748bd784f653e4bc
+[
++src/main/java/com/kk/wifi/HelloServlet.java,0\0\007f198a3f25863513e81cba4a21eaf707eebc82
--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -1,7 +1,41 @@
+/* 비동기적으로 현재 위치를 알아내어 지정된 요소에 출력한다. */
+function whereami() {
+  // 이 객체를 getCurrentPosition() 메서드의 세번째 인자로 전달한다.
+  var options = {
+    // 가능한 경우, 높은 정확도의 위치(예를 들어, GPS 등) 를 읽어오려면 true로 설정
+    // 그러나 이 기능은 배터리 지속 시간에 영향을 미친다.
+    enableHighAccuracy: false, // 대략적인 값이라도 상관 없음: 기본값
 
+    // 위치 정보가 충분히 캐시되었으면, 이 프로퍼티를 설정하자,
+    // 위치 정보를 강제로 재확인하기 위해 사용하기도 하는 이 값의 기본 값은 0이다.
+    maximumAge: 30000,     // 5분이 지나기 전까지는 수정되지 않아도 됨
+
+    // 위치 정보를 받기 위해 얼마나 오랫동안 대기할 것인가?
+    // 기본값은 Infinity이므로 getCurrentPosition()은 무한정 대기한다.
+    timeout: 15000    // 15초 이상 기다리지 않는다.
+  }
+
+  if(navigator.geolocation) // geolocation 을 지원한다면 위치를 요청한다.
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  else
+    console.log("이 브라우저에서는 Geolocation이 지원되지 않습니다.")
+
+
+  // geolocation 요청이 실패하면 이 함수를 호출한다.
+  function error(e) {
+    console.log("Geolocation 오류 "+e.code +": " + e.message);
+  }
+
+  function success(pos) {
+    document.getElementById("LAT").value = pos.coords.latitude
+    document.getElementById("LNT").value =   pos.coords.longitude
+
+  }
+}
 
 const getLocationEvent = () => {
   alert("Give Me Location!!");
+  whereami();
 
 }
 
@@ -18,6 +52,9 @@ function addEventToNearWifiBtn() {
   const nearWifiBtn = document.getElementById("near_wifi_btn");
   nearWifiBtn.addEventListener('click', getNearWifiEvent);
 }
+
+
+
 
 const init = () =>  {
   addEventToLocationBtn();
